6e027f8e701c5745b960e52c3d5ef1ff0c722432
rygenzx <maulana.zx525@gmail.com>
2020-07-22 17:25:41 +0700

    add file search movie app

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const searchMovie = () => {
+    const input = document.getElementById('input-cari');
+    const result = document.getElementById('result');
+    const apiKey = '49cd9371';
+    const endPoint = `http://www.omdbapi.com/?apikey=${apiKey}`;
+
+    input.addEventListener("keyup", async () => {
+            try {
+                const req = await fetch(`${endPoint}&s=${input.value}`)
+                const res = await req.json();
+                res.Search.forEach(movie => {
+            
+                result.innerHTML = `
+                <div class="card my-2" >
+              <img src="${movie.Poster}" class="card-img-top" alt="${movie.Title">
+              <div class="card-body">
+                <h5 class="card-title">${movie.Title}</h5>
+                <p class="card-text">${movie.Year}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>   `;
+          }  )                
+            } catch(error) {
+                console.log(error)
+            }
+    })
+};
+
+searchMovie()
